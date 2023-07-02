9bd04088bf20e566e389850000676792b352619b
Mashaan <mashaan.awad@outlook.com>
2023-07-02 19:04:38 +0300

    Update params.py

--- params.py
+++ params.py
@@ -2,18 +2,17 @@
 
 # params for training network
 num_gpu = 1
-num_epochs_pre = 2 #10
-log_step_pre = 20
-eval_step_pre = 20
+num_epochs_pre = 1000
 save_step_pre = 100
-num_epochs = 10 #100
+num_epochs = 1000
 log_step = 100
 save_step = 100
 manual_seed = None
 
 
 # params for optimizing models
-d_learning_rate = 1e-4
-c_learning_rate = 1e-4
+batch_size = 30
+d_lr = 1e-4 #1e-4
+g_lr = 1e-4 #1e-4
 beta1 = 0.5
 beta2 = 0.9
