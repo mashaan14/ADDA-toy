a8991c400f808919a8fe844b5651adc2d800b797
Mashaan <mashaan.awad@outlook.com>
2024-03-03 14:27:34 +0300

    fixing the returned model to be the best model instead of the last model.

--- core.py
+++ core.py
@@ -10,7 +10,6 @@ from sklearn.metrics.cluster import adjusted_rand_score
 import params
 from utils import make_variable
 
-
 def train_src(encoder, classifier, data_loader):
     """Train classifier for source domain."""
     ####################
@@ -20,53 +19,54 @@ def train_src(encoder, classifier, data_loader):
     # set train state for Dropout and BN layers
     encoder.train()
     classifier.train()
-    lossi = []
+    loss_e = []
+    loss_min = float('inf')
 
     # setup criterion and optimizer
-    optimizer = optim.Adam(
-        list(encoder.parameters()) + list(classifier.parameters()))
+    optimizer = optim.Adam(list(encoder.parameters()) + list(classifier.parameters()),
+                           lr=s_lr)
     criterion = nn.CrossEntropyLoss()
 
     ####################
     # 2. train network #
     ####################
 
-    for epoch in range(params.num_epochs_pre):
+    for epoch in range(num_epochs_pre):
+      loss_i = torch.zeros(len(XS_train_dataloader))
+      for step, (samples, labels) in enumerate(data_loader):
+        # Transfer to GPU
+        samples, labels = samples.to(device), labels.to(device)
+        # zero gradients for optimizer
+        optimizer.zero_grad()
+        # compute loss for encoder
+        loss = criterion(classifier(encoder(samples)), labels)
+        loss_i[step] = loss
+        # optimize source classifier
+        loss.backward()
+        optimizer.step()
 
-        for step, (samples, labels) in enumerate(data_loader):
-
-            # check if GPU is present
-            if torch.cuda.is_available():
-                samples = samples.cuda()
-                labels = labels.cuda()
-
-            # zero gradients for optimizer
-            optimizer.zero_grad()
-            # compute loss for encoder
-            loss = criterion(classifier(encoder(samples)), labels)
-            lossi.append(loss.item())
-            # optimize source classifier
-            loss.backward()
-            optimizer.step()
-
-        # print epoch info
-        if ((epoch + 1) % params.log_step == 0):
-            print("Epoch [{}/{}] Step [{}/{}]: loss={:.5f}"
-                    .format(epoch + 1,
-                            params.num_epochs_pre,
-                            step + 1,
-                            len(data_loader),
-                            loss.item()))
+      if loss_i.mean() < loss_min:
+        loss_min = loss_i.mean()
+        encoder_loss_min = encoder
+        classifier_loss_min = classifier
+      loss_e.append(loss_i.mean().item())
+      # print epoch info
+      if ((epoch + 1) % log_step == 0):
+          print("Epoch [{}/{}]: loss={:.4f}"
+                  .format(epoch + 1,
+                          num_epochs_pre,
+                          loss_i.mean().item()))
 
 
     fig = plt.figure()  # figsize=(6, 6)
     ax = fig.add_subplot(111)
     # plot the average loss over 100 epochs
-    plt.plot(torch.tensor(lossi).view(-1, params.log_step).mean(1))    
+    # plt.plot(torch.tensor(lossi).view(-1, 10).mean(1))
+    plt.plot(loss_e)
     plt.title('Training source loss')
     plt.savefig('loss.png', bbox_inches='tight', dpi=600)
 
-    return encoder, classifier
+    return encoder_loss_min, classifier_loss_min
 
 
 def eval_src(encoder, classifier, data_loader, fig_title=None):
@@ -152,109 +152,101 @@ def train_tgt(src_encoder, tgt_encoder, discriminator, classifier, src_data_load
     # 1. setup network #
     ####################
 
-    # set train state for Dropout and BN layers    
+    # set train state for Dropout and BN layers
     discriminator.train()
-    tgt_encoder.train()    
-    generator_lossi = []
-    discriminator_lossi = []
+    tgt_encoder.train()
+
+    d_loss_e = []
+    g_loss_e = []
+    g_loss_min = float('inf')
 
     # setup criterion and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer_discriminator = optim.Adam(discriminator.parameters(), lr=params.d_lr)
-                                        #  betas=(params.beta1, params.beta2))
-    optimizer_tgt = optim.Adam(tgt_encoder.parameters(), lr=params.g_lr)
-                            #    betas=(params.beta1, params.beta2))
-    # A scheduler to reduce the learning rate
-    scheduler_tgt = optim.lr_scheduler.MultiStepLR(optimizer_tgt,
-                                                   milestones=[round(params.num_epochs*0.3),round(params.num_epochs*0.6)], gamma=1e-1)
-    len_data_loader = min(len(src_data_loader), len(tgt_data_loader))
-
+    optimizer_discriminator = optim.Adam(discriminator.parameters(), lr=d_lr)
+    optimizer_tgt = optim.Adam(tgt_encoder.parameters(), lr=g_lr)
 
     ####################
     # 2. train network #
     ####################
 
-    for epoch in range(params.num_epochs):
-            
-        # zip source and target data pair
-        for step, ((samples_src, _), (samples_tgt, _)) in enumerate(zip(src_data_loader, tgt_data_loader)):
+    for epoch in range(num_epochs):
+      d_loss_i = torch.zeros(len(XT_train_dataloader))
+      g_loss_i = torch.zeros(len(XT_train_dataloader))
+      # zip source and target data pair
+      for step, ((samples_src, _), (samples_tgt, _)) in enumerate(zip(src_data_loader, tgt_data_loader)):
+        # Transfer to GPU
+        samples_src, samples_tgt = samples_src.to(device), samples_tgt.to(device)
 
-            # make images variable
-            if torch.cuda.is_available():
-                samples_src = samples_src.cuda()
-                samples_tgt = samples_tgt.cuda()
+        # extract and concat features
+        feat_src = src_encoder(samples_src)
+        feat_tgt = tgt_encoder(samples_tgt)
+        # detach feat_tgt from the tgt_encoder to avoid this error:
+        # RuntimeError: Trying to backward through the graph a second time
+        feat_concat = torch.cat((feat_src, feat_tgt.detach()), 0)
 
-            # extract and concat features
-            feat_src = src_encoder(samples_src)
-            feat_tgt = tgt_encoder(samples_tgt)
-            # detach feat_tgt from the tgt_encoder to avoid this error:
-            # RuntimeError: Trying to backward through the graph a second time
-            feat_concat = torch.cat((feat_src, feat_tgt.detach()), 0)
+        # prepare real and fake label
+        label_src = torch.ones(feat_src.size(0), requires_grad=False).long().to(device)
+        label_tgt = torch.zeros(feat_tgt.size(0), requires_grad=False).long().to(device)
+        adversary_label = torch.cat((label_src, label_tgt), 0)
 
-            # prepare real and fake label
-            label_src = make_variable(torch.ones(feat_src.size(0), requires_grad=False).long())
-            label_tgt = make_variable(torch.zeros(feat_tgt.size(0), requires_grad=False).long())
-            adversary_label = torch.cat((label_src, label_tgt), 0)
+        ###########################
+        # 2.1 train discriminator #
+        ###########################
 
-            ###########################
-            # 2.1 train discriminator #
-            ###########################
+        # clear out the gradients from the last step loss.
+        optimizer_discriminator.zero_grad()
+        # compute loss for discriminator
+        discriminator_loss = criterion(discriminator(feat_concat), adversary_label)
+        d_loss_i[step] = discriminator_loss
+        #backward propagation: calculate gradients
+        discriminator_loss.backward()
+        #update the weights
+        optimizer_discriminator.step()
 
-            # clear out the gradients from the last step loss.
-            optimizer_discriminator.zero_grad()            
-            # compute loss for discriminator
-            discriminator_loss = criterion(discriminator(feat_concat), adversary_label)
-            discriminator_lossi.append(discriminator_loss.item())
-            #backward propagation: calculate gradients
-            discriminator_loss.backward()
-            #update the weights
-            optimizer_discriminator.step()
+        ############################
+        # 2.2 train target encoder #
+        ############################
 
-            ############################
-            # 2.2 train target encoder #
-            ############################
+        # clear out the gradients from the last step loss
+        optimizer_tgt.zero_grad()
+        # compute loss for target encoder
+        generator_loss = criterion(discriminator(feat_tgt), 1 - label_tgt)
+        # generator_loss = criterion(discriminator(feat_concat.detach()), 1 - adversary_label)
+        g_loss_i[step] = generator_loss
+        # backward propagation: calculate gradients
+        generator_loss.backward()
+        # update the weights
+        optimizer_tgt.step()
 
-            # clear out the gradients from the last step loss
-            optimizer_tgt.zero_grad()
-            # compute loss for target encoder
-            generator_loss = criterion(discriminator(feat_tgt), 1 - label_tgt)
-            # generator_loss = criterion(discriminator(feat_concat.detach()), 1 - adversary_label)
-            generator_lossi.append(generator_loss.item())
-            # backward propagation: calculate gradients
-            generator_loss.backward()
-            # update the weights
-            optimizer_tgt.step()
-
-
-            
-
-        #######################
-        # 2.3 print epoch info #
-        #######################
-        if ((epoch + 1) % params.log_step == 0):
-            print("Epoch [{}/{}] Step [{}/{}]:"
-                    "d_loss={:.5f} g_loss={:.5f}"
-                    .format(epoch + 1,
-                            params.num_epochs,
-                            step + 1,
-                            len_data_loader,
-                            discriminator_loss.item(),
-                            generator_loss.item()))
-        
-        scheduler_tgt.step()
+      d_loss_e.append(d_loss_i.mean().item())
+      if g_loss_i.mean() < g_loss_min:
+        g_loss_min = g_loss_i.mean()
+        tgt_encoder_loss_min = tgt_encoder
+      g_loss_e.append(g_loss_i.mean().item())
+      #######################
+      # 2.3 print epoch info #
+      #######################
+      if ((epoch + 1) % log_step == 0):
+          print("Epoch [{}/{}]: d_loss={:.4f} g_loss={:.4f}"
+                  .format(epoch + 1,
+                          num_epochs,
+                          d_loss_i.mean().item(),
+                          g_loss_i.mean().item()))
 
     fig = plt.figure()  # figsize=(6, 6)
     ax = fig.add_subplot(111)
     # plot the average loss
-    plt.plot(torch.tensor(discriminator_lossi).view(-1, params.log_step).mean(1))
+    # plt.plot(torch.tensor(discriminator_lossi).view(-1, log_step).mean(1))
+    plt.plot(d_loss_e)
     plt.title('Discriminator loss')
     plt.savefig('discriminator_loss.png', bbox_inches='tight', dpi=600)
 
     fig = plt.figure()  # figsize=(6, 6)
     ax = fig.add_subplot(111)
     # plot the average loss
-    plt.plot(torch.tensor(generator_lossi).view(-1, params.log_step).mean(1))
+    # plt.plot(torch.tensor(generator_lossi).view(-1, log_step).mean(1))
+    plt.plot(g_loss_e)
     plt.title('Generator loss')
     plt.savefig('generator_loss.png', bbox_inches='tight', dpi=600)
 
-    return tgt_encoder
+    return tgt_encoder_loss_min
